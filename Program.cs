ed766bf6465b7f8b365e53553e43f645818290c7
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-06-14 20:18:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Text;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть число:");
+        double number = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть відсоток:");
+        double percentage = Convert.ToDouble(Console.ReadLine());
+
+        double result = number * (percentage / 100);
+
+        Console.WriteLine($"Результат: {result}");
+    }
+}
+
