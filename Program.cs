e671a4505848af18b2135278ab03763301c92749
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-06-15 19:22:12 +0300

    Apdate

--- Program.cs
+++ Program.cs
@@ -1,20 +1,53 @@
-﻿using System.Drawing;
-using System.Text;
-using System;
+﻿using System;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Console.WriteLine("Введіть число:");
-        double number = Convert.ToDouble(Console.ReadLine());
+        int[] numbers = new int[5];
 
-        Console.WriteLine("Введіть відсоток:");
-        double percentage = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введіть п'ять чисел:");
 
-        double result = number * (percentage / 100);
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Число {i + 1}: ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
 
-        Console.WriteLine($"Результат: {result}");
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int i = 1; i < 5; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+
+            if (numbers[i] < min)
+            {
+                min = numbers[i];
+            }
+        }
+
+        int product = 1;
+        foreach (int num in numbers)
+        {
+            product *= num;
+        }
+
+        Console.WriteLine($"Сума чисел: {sum}");
+        Console.WriteLine($"Максимум: {max}");
+        Console.WriteLine($"Минимум: {min}");
+        Console.WriteLine($"Добуток чисел: {product}");
+
+        Console.ReadLine();
     }
 }
 
